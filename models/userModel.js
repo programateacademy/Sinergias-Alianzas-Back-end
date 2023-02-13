3afcd8afb7a97c0fbbfbc29e05daa8d38c8d6c01
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-02-12 20:49:06 -0500

    Se agregan validaciones en el modelo de usuarios

--- models/userModel.js
+++ models/userModel.js
@@ -2,23 +2,54 @@
 const mongoose = require("mongoose");
 
 // Define the structure of the model
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true, minlength: 8},
-  id: { type: String },
-  admin:{
-    type: Boolean,
-    default: false,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Por favor ingresa el nombre"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Por favor ingresa el correo"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Ingresa un correo válido",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Por favor ingresa la contraseña"],
+      minlength: 8,
+    },
+
+    rol: {
+      type: String,
+      required: true,
+      default: "Colaborador",
+      // Otros roles: Admin, suspendido,
+    },
+
+    isVerify: {
+      type: Boolean,
+      default: false,
+    },
+
+    userAgent: {
+      type: Array,
+      required: true,
+      default: [],
+    },
   },
-  visible:{
-    type: Boolean,
-    default: true,
-  },
-  verify_token:{
-    type: String,
+
+  {
+    timestamps: true,
+    minimize: false,
   }
-});
+);
 
 // variable that will contain the model to be able to export
 const User = mongoose.model("User", userSchema);
